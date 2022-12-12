9679a8af85f71c98a0c425649edf593b9768f62f
Christy-Varghese <christy821vargese@gmail.com>
2022-12-12 13:39:16 -0500

    Maps Activity updated

--- app/src/main/java/group/example/project2/MapsActivity.kt
+++ app/src/main/java/group/example/project2/MapsActivity.kt
@@ -1,7 +1,15 @@
 package group.example.project2
 
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,39 +18,108 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import group.example.project2.databinding.ActivityMapsBinding
+import java.io.IOException
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
+    // current location
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val permissionCode = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getCurrentLocation()
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private fun getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                permissionCode
+            )
+            return
+        }
+        val getLocation =
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    currentLocation = location
+                    val mapFragment = supportFragmentManager
+                        .findFragmentById(R.id.map) as SupportMapFragment
+                    mapFragment.getMapAsync(this)
+                }
+            }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            permissionCode -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation()
+            }
+        }
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        val currentLoc = LatLng(currentLocation.latitude, currentLocation.longitude)
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.addMarker(
+            MarkerOptions().position(currentLoc).title("currentLoc - "+ getAddress(currentLoc))
+        )
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(currentLoc))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLoc, 12f))
+    }
+
+    private fun getAddress(latLng: LatLng): String {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        var curAddress: List<Address>? = null
+        try {
+            curAddress = geocoder.getFromLocation(latLng!!.latitude, latLng!!.longitude, 1)
+        } catch (e1: IOException) {
+            Log.e("Geocoding", "getString(R.string.problem)", e1)
+        } catch (e2: IllegalArgumentException) {
+            Log.e("Geocoding", "getString(R.string.invalid)"+
+                    "Latitude = " + latLng!!.latitude +
+                    ", Longitude = " +
+                    latLng!!.longitude, e2)
+        }
+        // If the reverse geocode returned an address
+        if (curAddress != null) {
+            // Get the first address
+            val address = curAddress[0]
+            val addressText = String.format(
+                "%s, %s, %s",
+                address.getAddressLine(0), // If there's a street address, add it
+                address.locality,                 // Locality is usually a city
+                address.countryName)              // The country of the address
+            return addressText
+        }
+        else
+        {
+            Log.e("Geocoding", "getString(R.string.noaddress)")
+            return ""
+        }
     }
 }
