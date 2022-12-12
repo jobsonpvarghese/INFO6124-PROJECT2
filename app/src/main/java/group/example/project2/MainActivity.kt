3e645d619e0ed95bcc3c1acd81433b2b5f03d140
jobsonpvarghese <jobsonvarghese1@gmail.com>
2022-12-12 16:08:58 -0500

    menu added with menu items

--- app/src/main/java/group/example/project2/MainActivity.kt
+++ app/src/main/java/group/example/project2/MainActivity.kt
@@ -1,11 +1,30 @@
 package group.example.project2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.home ->{
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
